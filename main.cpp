1495a9247215e2a96f3ea08baed29b1296fd57ca
pagrawal1407 <pagrawal@mail.csuchico.edu>
2016-02-08 11:01:56 -0800

    Last commit

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 int main()
 {
   int n, rollnum;
+  char ch = 'Y';
 
   cout << "Enter the number of faces of the Die: ";
   cin >> n;
 
   GameDie D (n);
-  rollnum = D.roll();
-  cout << endl << "Rolled number is: " << rollnum;
-return 0;
+  while (ch != 'N')
+  {
+     rollnum = D.roll();
+     cout << endl << "Rolled number is: "<<rollnum << endl;
+     cout << "Do you want to roll again? (Y/N): ";
+     cin >> ch;
+  }
+   return 0;
+
 }
